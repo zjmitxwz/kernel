95426e6121fb087c8322cf928709061b1fd3f5f8
hech <hech@163.com>
2023-10-24 15:58:54 +0800

    修改编译脚本

--- build.sh
+++ build.sh
@@ -7,6 +7,9 @@ args=" -j6 \
         NM=${clang}/llvm-nm \
         OBJDUMP=${clang}/llvm-objdump \
         STRIP=${clang}/llvm-strip \
+	CROSS_COMPILE_ARM32=${clang}/arm-linux-gnueabi- \
+	LLVM=1 \
+	LLVM_IAS=1 \
 	PYTHON=python3
 "
 make $args umi_defconfig
