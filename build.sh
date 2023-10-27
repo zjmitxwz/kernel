a5a8e0ad41752cb1f91b5c8a22e5845fd3cdf326
hech <zitxwz@163.com>
2023-10-27 02:40:47 +0000

    修改

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+os="miui"
 clang="/home/hech/clang/clang1800/bin"
 args=" -j6 \
         ARCH=arm64 \
@@ -14,3 +15,22 @@ args=" -j6 \
 "
 make $args umi_defconfig
 make $args
+
+if [ -f $PWD/out/arch/arm64/boot/Image ]; then
+	cp $PWD/out/arch/arm64/boot/Image $PWD/AnyKernel3/kernels/${os}/
+else
+	exit -1;
+fi;
+
+#if [ -f $PWD/out/arch/arm64/boot/dtbo.img ]; then
+#	cp $PWD/out/arch/arm64/boot/dtbo.img $PWD/AnyKernel3/kernels/${os}/dtbo.img
+#fi;
+
+#if [ -f $PWD/out/arch/arm64/boot/dts/vendor/qcom/kona-v2.1.dtb ]; then
+#	cp $PWD/out/arch/arm64/boot/dts/vendor/qcom/kona-v2.1.dtb $PWD/AnyKernel3/kernels/${os}/dtb
+#fi;
+
+cd $PWD/AnyKernel3
+zip -r9 ${os}-AnyKernel3.zip * -x .git README.md
+cp ${os}-AnyKernel3.zip ..
+
